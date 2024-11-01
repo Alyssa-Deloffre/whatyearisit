1aefc5e1fb595acc2b278e8324245bc5f5f18be2
Alyssa Deloffre <alyssa.deloffre@gmail.com>
2024-11-01 11:34:50 +0100

    fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getYear(){
     fetch('https://whatyearisit-backend-snowy-seven.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-        document.querySelector("#year").innerHTML += data.year
+        document.querySelector("#year").innerHTML += data.year.toString()
     })
 }
 
