9e81d1ec62782e0ca6807317d2edf8fa8a87450a
Alyssa Deloffre <alyssa.deloffre@gmail.com>
2024-11-01 11:10:12 +0100

    change url in script js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function getYear(){
-    fetch('http://localhost:3000/year')
+    fetch('https://whatyearisit-backend-two-henna.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector("#year").innerHTML += data.year
